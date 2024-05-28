4b6732197bde366f653ea9515a7ba86131a3c4cc
Daniel Jaimes <danielDAJJ@outlook.com>
2024-05-27 20:30:53 -0500

    consumientod apis

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import NavBarComponent from './components/NavBarComponent/NavBarComponent';
 import ItemListContainerComponent from './components/ItemListContainerComponent/ItemListContainerComponent';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import axios, {isCancel, AxiosError} from 'axios';
 
 const App = () => {
+  
   return(
     <>
     <NavBarComponent/>
--- src/components/ItemListContainerComponent/ItemListContainerComponent.jsx
+++ src/components/ItemListContainerComponent/ItemListContainerComponent.jsx
@@ -1,19 +1,33 @@
 import { width } from '@fortawesome/free-brands-svg-icons/fa42Group'
 import React from 'react'
+import { getAllProducts } from '../../../services/ProductsServices'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const ItemListContainerComponent = ({greeting}) => {
-    const customStyle = {
-        color: 'white',
-        fontSize: '3rem',
-        width: '100dvw',
-        height: '100dvh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 'auto'
-    }
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(() => {
+      getAllProducts()
+        .then((res) => setProducts(res.data.products))
+        .catch((err) => console.log(err))
+    }, [])
+
   return (
-    <div style={customStyle}>{greeting}</div>
+    <div className='ItemListContainer'>
+      {products.map((product) => {
+        return(
+          <Card key={product.id} style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={product.thumbnail}/>
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>
+              {product.description}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      )})}
+    </div>
   )
 }
 
